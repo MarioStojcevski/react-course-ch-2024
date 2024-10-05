44e18b08f24ced800066aebb8dfef874702cf7b2
Mario Stojcevski <mane.sto@outlook.com>
2024-10-05 15:59:01 +0200

    [02 React Components] Finishing 02 components app

--- 02-react-components-jsx-state/src/App.jsx
+++ 02-react-components-jsx-state/src/App.jsx
@@ -1,25 +1,37 @@
 import reactLogo from './assets/react.svg';
+import libraries from './utils/libraries';
 import angularLogo from '/angular.png';
+import Card from "./components/Card";
 import viteLogo from '/vite.svg';
 import qwikLogo from '/qwik.png';
-
 import './App.css';
 
-import Slikicka from "./components/Slikicka";
-
 function App() {
-  const brojce = 5;
-
   return (
     <div>
-      <h1>Vite + React + Angular + Qwik</h1>
+      <h1 style={{ color: '#545454' }}>Vite + React + Angular + Qwik</h1>
 
-      <div>
-        {brojce === 3 && <p>brojce e stvarno 3</p>}
-        <Slikicka logo={viteLogo} url={"https://www.google.com"} alt={"www.google.com"} />
-        <Slikicka logo={reactLogo} url={"www.facebook.com"} alt={"www.facebook.com"} />
-        <Slikicka logo={angularLogo} url={"www.instagram.com"} alt={"www.instagram.com"} />
-        <Slikicka logo={qwikLogo} url={"www.pinterest.com"} alt={"www.pinterest.com"} />
+      <div style={{ display: 'inline-flex'}}>
+        <Card 
+          name={'vite'} 
+          logo={viteLogo}
+          url={libraries.vite.url} 
+          alt={libraries.vite.description} />
+        <Card 
+          name={'react'} 
+          logo={reactLogo} 
+          url={libraries.react.url} 
+          alt={libraries.react.description} />
+        <Card 
+          name={'angular'} 
+          logo={angularLogo} 
+          url={libraries.angular.url} 
+          alt={libraries.angular.description} />
+        <Card 
+          name={'qwik'} 
+          logo={qwikLogo} 
+          url={libraries.qwik.url} 
+          alt={libraries.qwik.description} />
       </div>
       
     </div>
