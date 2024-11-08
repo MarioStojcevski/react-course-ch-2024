2199e96192eb825cbf2d4cb6b85643069e2d3551
Mario Stojcevski <mane.sto@outlook.com>
2024-11-08 19:12:21 +0100

    React refs and portals

--- 07-react-portals-and-refs/src/components/ResultModal.jsx
+++ 07-react-portals-and-refs/src/components/ResultModal.jsx
@@ -1,11 +1,24 @@
-import { createPortal } from 'react-dom';
+import { forwardRef, useRef, useImperativeHandle } from "react";
 
-const ResultModal = () => {
-  return createPortal(
-    <dialog open>
-      <h2>Result</h2>
+const ResultModal = forwardRef(function ResultModal ({ result, targetTime}, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      dialog.current.showModal();
+    }
+  }));
+
+  return (
+    <dialog ref={dialog} className='result-modal'>
+      <h2>You {result}</h2>
+      <p>The target time was <strong>{targetTime}</strong> seconds.</p>
+      <p>You stopped the timer with <strong>X seconds left.</strong></p>
+      <form method="dialog">
+        <button>Close</button>
+      </form>
     </dialog>
-  , document.body);
-};
+    );
+});
 
 export default ResultModal;
--- 07-react-portals-and-refs/src/components/TimerChallenge.jsx
+++ 07-react-portals-and-refs/src/components/TimerChallenge.jsx
@@ -4,32 +4,39 @@ import ResultModal from "./ResultModal";
 const TimerChallenge = ({ title, targetTime }) => {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
-  const timer = useRef(null);
+  const timer = useRef();
+  const dialog = useRef();
 
   const startTimer = () => {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
+      dialog.current.open();
     }, targetTime * 1000);
-    
+
     setTimerStarted(true);
+  }
+
+  const handleStop =() => {
+    clearTimeout(timer.current);
   }
 
   return (
     <>
-      <ResultModal />
+      <ResultModal ref={dialog} targetTime={targetTime} result="lost" />
 
       <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p className="challenge-failed">Time's up!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={startTimer}>
+        <button onClick={timerStarted ? handleStop : startTimer}>
           {timerStarted ? "Stop" : "Start"}
         </button>
       </p>
 
-      <p className={timerStarted ? "active" : undefined}>
+      <p className={timerStarted ? 'active' : undefined}>
         {timerStarted ? "Timer Running" : "Timer Not Started"}
       </p>
     </section>
